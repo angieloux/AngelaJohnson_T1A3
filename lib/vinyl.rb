fdd40b597059c72626d7971f888b05c12df93932
ANGIELOUX <angelajohnson943@gmail.com>
2021-10-03 01:26:00 +1000

    Add error handling for price search
    This commit:
    - Fixes previous issue of program not recognising when invalid
      input was entered when searching by price
    - Now presents a hint to the user that all records are over $40 if
      they type anything other than numbers eq to or greater than 40

--- lib/vinyl.rb
+++ lib/vinyl.rb
@@ -50,16 +50,23 @@ module VinylSection
     search_by_keyword("Artist")
     when 3
     search_by_keyword("Album")
-    when 4
-      puts "Whats the max you're willing to pay for a record?"
-      price = gets.to_i
-      system 'clear'
-      for record in STOCK
-        if record["Price"] <= price
-            display_filtered_records(record)
+    when 4 
+        found = false
+        while !found
+          puts "Whats the max you're willing to pay for a record?"
+          price = gets.to_i
+          system 'clear'
+          if (..40).include?(price)
+            puts "[What was I thinking, I haven't seen a single record here for less than ".italic + "40 bucks. ".magenta.italic + "Guess I've gotta stretch the purse strings... :'( ]".italic
+          end
+          for record in STOCK
+            if record["Price"] <= price
+                display_filtered_records(record)
+                found = true
+            end
+          end
         end
-      end
-      @@cart.add_items_to_cart
+        @@cart.add_items_to_cart
 
     # LOOK AROUND THE STORE **********
     when 5
@@ -72,7 +79,7 @@ module VinylSection
       menu.choice "Look for a specific GENRE", 1
       menu.choice "Look for a specific ARTIST", 2
       menu.choice "Look for a specific ALBUM", 3
-      menu.choice "Look for a specific PRICE", 4
+      menu.choice "Set a maximum PRICE", 4
       menu.choice "Go back", 5
     end
     filter_records(input)
